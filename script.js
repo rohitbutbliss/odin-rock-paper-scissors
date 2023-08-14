dd86794a781a29234e019835bb3f4129818d8c2a
bliss <bliss@Rohits-MacBook-Air.local>
2023-08-14 18:11:58 +0530

    Added UI to the project - added interactice ui to the project - used images & gif to make project looks visually interesting - used google fonts to add font style as well;

--- script.js
+++ script.js
@@ -1,6 +1,78 @@
+let options = document.querySelectorAll(".option");
+let earthsChoiceDiv = document.querySelector(".earth-choice > div");
 
-// this function gives us random value chosen by computer
-function getComputerChoice() {
+let earthScore = 0;
+let alienScore = 0;
+
+options.forEach(option => option.addEventListener("click", startRound));
+
+function startRound(e) {
+    let earthsChoice;
+    let aliensChoice = getAlienChoice();
+
+    if (e.target === e.currentTarget) {
+        imageUrl = e.target.querySelector("img").getAttribute("src");
+        earthsChoice = e.target.querySelector("img").getAttribute("alt");
+    }
+
+    else {
+        imageUrl = e.target.getAttribute("src");
+        earthsChoice = e.target.getAttribute("alt");
+    }
+
+    setImageUrl(earthsChoice, aliensChoice);
+    updateScore(earthsChoice, aliensChoice);
+    setScore();
+
+};
+
+function updateScore(earthsChoice, aliensChoice) {
+    let result = playRound(earthsChoice, aliensChoice);
+    if (result === 1) earthScore++;
+    else if (result === 0) alienScore++;
+
+    if (earthScore === 5 || alienScore === 5) {
+        options.forEach(option => option.removeEventListener("click", startRound));
+        endGame();
+    }
+}
+
+function endGame() {
+    let endText = document.querySelector(".ending-text");
+    if (alienScore === 5) {
+        endText.textContent = "In the aftermath of the invasion, Earth now bears the mark of an alien world, a stark reminder of the cost of division and the need for unity in the face of the unknown.";
+    }
+    else {
+        endText.textContent = "Against all odds, Earth united and triumphed, securing a future where humanity's resilience shines brighter than the stars.";
+
+    }
+}
+
+function setScore() {
+    let earthsScoreCard = document.querySelector(".earth-score > .score");
+    let aliensScoreCard = document.querySelector(".alien-score > .score");
+    earthsScoreCard.innerText = earthScore;
+    aliensScoreCard.innerText = alienScore;
+}
+
+function setImageUrl(earthsChoiceImg, aliensChoiceImg) {
+    let earthImage = document.createElement("img");
+    earthImage.src = `./images/${earthsChoiceImg}.png`;
+
+    let alienImage = document.createElement("img");
+    alienImage.src = `./images/${aliensChoiceImg}.png`;
+
+    let earthsChoiceDiv = document.querySelector(".earth-choice > div");
+    let aliensChoiceDiv = document.querySelector(".alien-choice > div");
+
+    earthsChoiceDiv.innerHTML = '';
+    aliensChoiceDiv.innerHTML = '';
+
+    earthsChoiceDiv.appendChild(earthImage);
+    aliensChoiceDiv.appendChild(alienImage);
+}
+
+function getAlienChoice() {
     // 1 - rock
     // 2 - paper
     // 3 - scissors
@@ -12,38 +84,12 @@ function getComputerChoice() {
 }
 
 
-// this function is used to simulate one round
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase().trim(); // converting player selection to lowercase & trimming in case extra space is there
-    if (!verifyPlayerSelection(playerSelection)) return "You provided wrong value";// if player has provided wrong input, we will return this
-
-    // if player has provided right input, we will simply return following values based on selection made by user & computer
-    if (playerSelection === computerSelection) return "Tied! Both cancel out each other";
-    else if (playerSelection === "rock" && computerSelection === "paper") return "You Lose! Paper beats Rock";
-    else if (playerSelection === "rock" && computerSelection === "scissors") return "You Won! Rock beats Scissors";
-    else if (playerSelection === "paper" && computerSelection === "rock") return "You Won! Paper beats Rock";
-    else if (playerSelection === "paper" && computerSelection === "scissors") return "You Lost! Scissors beats Paper";
-    else if (playerSelection === "scissors" && computerSelection === "rock") return "You Lost! Rock beats Scissors";
-    else if (playerSelection === "scissors" && computerSelection === "paper") return "You won! Scissors beats Paper";
+function playRound(earthsSelection, aliensSelection) {
+    if (earthsSelection === aliensSelection) return -1;
+    else if (earthsSelection === "rock" && aliensSelection === "paper") return 0;
+    else if (earthsSelection === "rock" && aliensSelection === "scissors") return 1;
+    else if (earthsSelection === "paper" && aliensSelection === "rock") return 1;
+    else if (earthsSelection === "paper" && aliensSelection === "scissors") return 0;
+    else if (earthsSelection === "scissors" && aliensSelection === "rock") return 0;
+    else if (earthsSelection === "scissors" && aliensSelection === "paper") return 1;
 }
-
-// this function checks if the value provided by user is correct or not
-function verifyPlayerSelection(playerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") return true;
-    return false;
-}
-
-// this program gathers both players' selection & calls for the round
-function game() {
-    const playerSelection = prompt("Please enter your exact choice[case insensitive] (rock or paper or scissors only):");
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-}
-
-// calling game function five times to simulate five rounds
-game();
-game();
-game();
-game();
-game();
